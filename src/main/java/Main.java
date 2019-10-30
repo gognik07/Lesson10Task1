3ed0c4bc2037169784080e1818725575dcaf3cca
gognik <gognik1990@gmail.com>
2019-10-30 16:19:29 +0300

    fix: исправлен запрос и текстовка

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -9,20 +9,22 @@ public class Main {
         String user = "root";
         String password = "gog69nik69";
 
-        String query = "SELECT Courses.name AS 'Название курса', COUNT(*) AS 'Количество учащихся за месяц',\n" +
-                "       CONCAT(MONTHNAME(registration_date), ' ', YEAR(registration_date)) AS 'Дата поступления' FROM Courses\n" +
-                "           JOIN Subscriptions ON Courses.id = Subscriptions.course_id\n" +
-                "           JOIN Students ON Students.id = Subscriptions.student_id\n" +
-                "GROUP BY CONCAT(MONTHNAME(registration_date), ' ', YEAR(registration_date)), Courses.name";
+        String query = "SELECT nameCourse, AVG(countMonth) AS 'average' FROM (\n" +
+                "         SELECT Courses.name AS 'nameCourse', COUNT(*) AS 'countMonth',\n" +
+                "                CONCAT(MONTHNAME(registration_date), ' ', YEAR(registration_date)) AS 'dateRegistration' FROM Courses\n" +
+                "                  JOIN Subscriptions ON Courses.id = Subscriptions.course_id\n" +
+                "                  JOIN Students ON Students.id = Subscriptions.student_id\n" +
+                "         GROUP BY CONCAT(MONTHNAME(registration_date), ' ', YEAR(registration_date)), Courses.name) AS tab\n" +
+                "GROUP BY nameCourse";
 
         Connection connection = DriverManager.getConnection(url, user, password);
         Statement statement = connection.createStatement();
         ResultSet result = statement.executeQuery(query);
 
         while (result.next()) {
-            System.out.println("Курс: " + result.getString("Название курса") +
-                    " в " + result.getString("Дата поступления") +
-                    " покупали в среднем " + result.getString("Количество учащихся за месяц"));
+           // System.out.println("Курс " + result.getString("nameCourse") +
+           //         " покупали в среднем " + result.getString("average") + " раз");
+            System.out.printf("Курс %s покупали в среднем %.1f раз\n", result.getString("nameCourse"), Double.parseDouble(result.getString("average")));
         }
     }
 }
